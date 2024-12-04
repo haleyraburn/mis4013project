188d42e324952421153933d9f4e9217c8fa98714
haleyraburn <raburnhaley@ou.edu>
2024-12-04 13:44:28 -0600

    Update model-swimmers.php

--- model-swimmers.php
+++ model-swimmers.php
@@ -2,7 +2,7 @@
 function selectSwimmers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM 'swimmer'");
+        $stmt = $conn->prepare("SELECT swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM swimmer");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
