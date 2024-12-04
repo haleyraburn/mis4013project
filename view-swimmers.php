2f5b2e2ec5cb3e5233ac67d0651caced7c1c14dd
haleyraburn <raburnhaley@ou.edu>
2024-12-04 13:42:44 -0600

    Create view-swimmers.php

---
+++ view-swimmers.php
@@ -0,0 +1,25 @@
+<h1>Roster</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Age</th>
+      <th>Gender</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+      while ($swimmer = $swimmers->fetch_assoc()) {
+    ?>
+        <tr>
+          <td><?php echo $swimmer['swimmer_id']; ?></td>
+          <td><?php echo $swimmer['swimmer_name']; ?></td>
+          <td><?php echo $swimmer['swimmer_age']; ?></td>
+          <td><?php echo $swimmer['swimmer_gender']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+  </tbody>
+</table>
