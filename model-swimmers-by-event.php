2fada0413b4c3935b61c112e15865e0727c9428b
haleyraburn <raburnhaley@ou.edu>
2024-12-05 14:11:37 -0600

    Update model-swimmers-by-event.php

--- model-swimmers-by-event.php
+++ model-swimmers-by-event.php
@@ -2,7 +2,7 @@
 function selectSwimmersByEvent($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT s.swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM swimmer s join event e on e.event_id = s.event_id WHERE s.swimmer_id=?");
+        $stmt = $conn->prepare("SELECT s.swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM swimmer s join event e on e.event_id = s.event_id WHERE s.event_id=?");
         $stmt->bind_param("i", $sid);
         $stmt->execute();
         $result = $stmt->get_result();
