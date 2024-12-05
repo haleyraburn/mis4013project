bc622e4e4270bbf016eca2af2c9099fd6822b1b6
haleyraburn <raburnhaley@ou.edu>
2024-12-05 14:30:27 -0600

    Create model-swimmers-by-meet.php

---
+++ model-swimmers-by-meet.php
@@ -0,0 +1,16 @@
+<?php
+function selectSwimmersByMeet($swid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT s.swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM swimmer s join meet m on m.meet_id = s.meet_id WHERE s.meet_id=?");
+        $stmt->bind_param("i", $swid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
