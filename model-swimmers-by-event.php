243f143c3fd9dbc5a0ff0223e92530c25d501f83
haleyraburn <raburnhaley@ou.edu>
2024-12-05 13:29:39 -0600

    Create model-swimmers-by-event.php

---
+++ model-swimmers-by-event.php
@@ -0,0 +1,16 @@
+<?php
+function selectSwimmersByEvent($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT s.swimmer_id, swimmer_name FROM swimmer");
+        $stmt->bind_param("i", $sid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
