63bf5b18d9730d2246c155c7e701397e6383cfa1
haleyraburn <raburnhaley@ou.edu>
2024-12-05 15:39:49 -0600

    Update view-meet-info.php

--- view-meet-info.php
+++ view-meet-info.php
@@ -4,9 +4,9 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">2024 Texas A&M Fall Invitational</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">If you are interested in competing in this meet, click the links below.</p>
         <a href="https://file.groupme.com/v1/101952913/files/8554a2a8-3756-46d8-880a-b784b34626e7?access_token=BksWXzskDJWCUeHoGLZrq7cAp36AyqO8Av0n8JPv&omit-content-disposition=true" class="btn btn-primary">Meet Packet</a>
-        <a href="#" class="btn btn-primary">Sign Up</a>
+        <a href="https://docs.google.com/forms/d/1-YYZ1BEvB5xdB4i6p86inQjE61Xa-sNnwlFwWyY_ReU/edit" class="btn btn-primary">Sign Up</a>
       </div>
     </div>
   </div>
@@ -14,9 +14,9 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">2024 OSU Spooky Invite Swim Meet</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">If you are interested in competing in this meet, click the links below.</p>
         <a href="https://file.groupme.com/v1/101952913/files/6fa471a7-8140-4f6f-9c77-5bb800c96b43?access_token=BksWXzskDJWCUeHoGLZrq7cAp36AyqO8Av0n8JPv&omit-content-disposition=true" class="btn btn-primary">Meet Packet</a>
-        <a href="#" class="btn btn-primary">Sign Up</a>
+        <a href="https://docs.google.com/forms/d/1IaGQtLeMCekd5V3gkDejtVMLCCZu_C9irKoFzSEyqaM/edit?usp=forms_home&ths=true" class="btn btn-primary">Sign Up</a>
       </div>
     </div>
   </div>
@@ -24,9 +24,9 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Harding University Bison Swim Meet</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">If you are interested in competing in this meet, click the links below.</p>
         <a href="https://file.groupme.com/v1/101952913/files/638c3e52-2505-4d7b-bb11-f6f9a79ef7b7?access_token=BksWXzskDJWCUeHoGLZrq7cAp36AyqO8Av0n8JPv&omit-content-disposition=true" class="btn btn-primary">Meet Packet</a>
-        <a href="#" class="btn btn-primary">Sign Up</a>
+        <a href="https://docs.google.com/forms/d/14S05ABLbCQuKoEWhTioUEazVX6QyfkpRBbE_bIQuLvE/edit" class="btn btn-primary">Sign Up</a>
       </div>
     </div>
   </div>
