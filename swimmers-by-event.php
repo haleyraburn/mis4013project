90bc576180bdee1292db1171a51a6b8b6bd5c67c
haleyraburn <raburnhaley@ou.edu>
2024-12-05 13:37:23 -0600

    Update swimmers-by-event.php

--- swimmers-by-event.php
+++ swimmers-by-event.php
@@ -4,7 +4,7 @@ require_once("model-swimmers-by-event.php");
 
 $pageTitle = "Swimmers in this Event";
 include "view-header.php";
-$swimmers = selectSwimmersbyEvent($_GET['sname']);
+$swimmers = selectSwimmersbyEvent($_GET['name']);
 include "view-swimmers-by-event.php";
 include "view-footer.php";
 ?>
