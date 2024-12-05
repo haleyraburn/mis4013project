fb9bc98ca23d9aa1c42d242ce69c7f2df2372780
haleyraburn <raburnhaley@ou.edu>
2024-12-05 14:28:59 -0600

    Create swimmers-by-meet.php

---
+++ swimmers-by-meet.php
@@ -0,0 +1,9 @@
+require_once("util-db.php");
+require_once("model-swimmers-by-meet.php");
+
+$pageTitle = "Swimmers competing in this Meet";
+include "view-header.php";
+$swimmers = selectSwimmersbyMeet($_GET['id']);
+include "view-swimmers-by-meet.php";
+include "view-footer.php";
+?>
