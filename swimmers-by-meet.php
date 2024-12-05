62a42739439655281294ebcfd170ff902ada515a
haleyraburn <raburnhaley@ou.edu>
2024-12-05 14:30:39 -0600

    Update swimmers-by-meet.php

--- swimmers-by-meet.php
+++ swimmers-by-meet.php
@@ -3,7 +3,7 @@ require_once("model-swimmers-by-meet.php");
 
 $pageTitle = "Swimmers competing in this Meet";
 include "view-header.php";
-$swimmers = selectSwimmersbyMeet($_GET['id']);
+$swimmers = selectSwimmersbyMeet($_GET['sid']);
 include "view-swimmers-by-meet.php";
 include "view-footer.php";
 ?>
