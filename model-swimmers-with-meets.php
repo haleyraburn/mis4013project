77cf07497cd0801c5b44c84eafa899ef74f7fff9
haleyraburn <raburnhaley@ou.edu>
2024-12-05 14:59:56 -0600

    Update model-swimmers-with-meets.php

--- model-swimmers-with-meets.php
+++ model-swimmers-with-meets.php
@@ -12,4 +12,19 @@ function selectSwimmers() {
         throw $e;
     }
 }
+
+function selectSwimmersByEvent($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT s.swimmer_id, swimmer_name, swimmer_age, swimmer_gender FROM swimmer s join event e on e.event_id = s.event_id WHERE s.event_id=?");
+        $stmt->bind_param("i", $sid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
