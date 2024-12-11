194afb7a66414baccf2cb154294f91f7b25b3b94
haleyraburn <raburnhaley@ou.edu>
2024-12-11 17:44:40 -0600

    Update model-swimmers.php

--- model-swimmers.php
+++ model-swimmers.php
@@ -12,4 +12,46 @@ function selectSwimmers() {
         throw $e;
     }
 }
+
+function insertSwimmer($sName, $sAge, $sGender) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO swimmer (swimmer_name, swimmer_age, swimmer_gender) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $sName, $sAge, $sGender);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSwimmer($sName, $sAge, $sGender, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update swimmer set swimmer_name = ?, swimmer_age = ?, swimmer_gender = ? where swimmer_id = ?");
+        $stmt->bind_param("sssi", $sName, $sAge, $sGender, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSwimmer($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from swimmer where swimmer_id = ?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
