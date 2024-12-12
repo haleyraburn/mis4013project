416ca2e75a52be8e1950bb59dc60a3e103e98db5
haleyraburn <raburnhaley@ou.edu>
2024-12-12 13:12:31 -0600

    Update and rename view-swimmer-input-list.php to view-gender-input-list.php

--- view-swimmer-input-list.php
+++
@@ -1,9 +0,0 @@
-<select class="form-select" id="sName" name="sName">
-<?php
-while ($swimmerItem = $swimmerList->fetch_assoc()) {
-?>
-  <option value="<?php echo $swimmerItem['swimmer_id']; ?>"><?php echo $swimmerItem['swimmer_name']; ?></option>
-<?php
-}
-?>
-</select>
