35e927bcdaea9c8f6b0448475042aeb56e8c002d
haleyraburn <raburnhaley@ou.edu>
2024-12-12 12:59:35 -0600

    Update view-swimmers-newform.php

--- view-swimmers-newform.php
+++ view-swimmers-newform.php
@@ -1,5 +1,3 @@
-
-
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newSwimmerModal">
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -19,7 +17,10 @@
         <form method ="post" action="">
           <div class="mb-3">
             <label for="sName" class="form-label">Name</label>
-            <input type="text" class="form-control" id="sName" name="sName">
+            <?php
+              $swimmerList = selectSwimmerForInput();
+              include "view-swimmer-input-list.php";
+            ?>
           </div>
           <div class="mb-3">
             <label for="sAge" class="form-label">Age</label>
