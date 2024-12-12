7461836d74356a3ab5e5b5a5bcd6f70dc07a0106
haleyraburn <raburnhaley@ou.edu>
2024-12-12 13:43:55 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>OU Club Swimming</title>
+    <link href="https://cdn.freebiesupply.com/logos/large/2x/oklahoma-sooners-2-logo-png-transparent.png" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     <div class="container">
