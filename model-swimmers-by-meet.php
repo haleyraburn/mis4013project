208693092d577cfc05984147d4e32eb55a014f31
haleyraburn <raburnhaley@ou.edu>
2024-12-12 13:15:37 -0600

    Update model-swimmers-by-meet.php

--- model-swimmers-by-meet.php
+++ model-swimmers-by-meet.php
@@ -13,4 +13,18 @@ function selectSwimmersByMeet($swid) {
         throw $e;
     }
 }
+
+function selectSwimmerForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT swimmer_id, swimmer_name FROM swimmer ORDER BY swimmer_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
