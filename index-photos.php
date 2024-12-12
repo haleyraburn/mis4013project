6c2f3d33fd873d87540a962cdf287872030843f4
haleyraburn <raburnhaley@ou.edu>
2024-12-12 14:19:19 -0600

    Update index-photos.php

--- index-photos.php
+++ index-photos.php
@@ -4,16 +4,4 @@
     <title>OU Swim Club Photos</title>
     <link rel="stylesheet" href="styles.css">
 </head>
-<body>
-    <div class="photo-grid">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4414.jpg" alt="Image #1">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4413.jpg" alt="Image #2">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4417.jpg" alt="Image #3">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4418.jpg" alt="Image #4">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4415.jpg" alt="Image #5">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4419.jpg" alt="Image #6">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4412.jpg" alt="Image #7">
-        <img src="C:\Users\Haley Raburn\Downloads\thumbnail_IMG_4416.jpg" alt="Image #8">
-    </div>  
-</body>
 </html>
