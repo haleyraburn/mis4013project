4a22791fb8df945cad565df24687098bfdab03bf
haleyraburn <raburnhaley@ou.edu>
2024-12-11 18:42:15 -0600

    Update swimmers.php

--- swimmers.php
+++ swimmers.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error adding swimmer.</div>';
         }
         break;
+      case "Delete":
+        if (deleteSwimmer($_POST['sid'])) {
+        echo '<div class="alert alert-success" role="alert">Swimmer deleted.</div>';
+        } else {
+        echo '<div class="alert alert-danger" role="alert">Error deleting swimmer.</div>';
+        }
+        break;
     }
   }
 $swimmers = selectSwimmers();
