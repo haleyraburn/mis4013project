5b720f1d863cc5d6c2a276afd55b207e1d86538a
haleyraburn <raburnhaley@ou.edu>
2024-12-12 13:23:07 -0600

    Delete view-gender-input-list.php

--- view-gender-input-list.php
+++
@@ -1,9 +0,0 @@
-<select class="form-select" id="sGender" name="sGender">
-<?php
-while ($genderItem = $genderList->fetch_assoc()) {
-?>
-  <option value="<?php echo $genderItem['swimmer_gender']; ?>"><?php echo $genderItem['swimmer_gender']; ?></option>
-<?php
-}
-?>
-</select>
