7b84a0558b3b2b938264a9be4ff499a3eb0edf10
haleyraburn <raburnhaley@ou.edu>
2024-12-11 18:14:54 -0600

    Update swimmers.php

--- swimmers.php
+++ swimmers.php
@@ -4,6 +4,14 @@ require_once("model-swimmers.php");
 
 $pageTitle = "Roster";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+      case "Add":
+        insertSwimmer($_POST['sName'], $_POST['sAge'], $_POST['sGender']);
+        break;
+    }
+  }
 $swimmers = selectSwimmers();
 include "view-swimmers.php";
 include "view-footer.php";
