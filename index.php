08779d459855eb47edd9c8882d83b0bab3102c92
haleyraburn <raburnhaley@ou.edu>
2024-12-11 19:20:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 include "view-header.php";
 ?>
 <h1>OU Club Swimming</h1>
+  <div class="grid-container">
+    <img src="https://www.instagram.com/ouswimclub/p/DCc2x2wST8D/" alt="Photo 1">
+    <img src="" alt="Photo 2">
+    <img src="" alt="Photo 3">
+    <img src="" alt="Photo 4">
+    <img src="" alt="Photo 5">
+    <img src="" alt="Photo 6">
+    <img src="" alt="Photo 7">
+    <img src="" alt="Photo 8">
+  </div>
 <?php
 include "view-footer.php";
 ?>
